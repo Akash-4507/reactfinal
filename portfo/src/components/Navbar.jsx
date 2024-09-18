57e481f61eac68780cbc79ec7c8f54ad0c60b029
Akash-4507 <akashanand9790@gmail.com>
2024-09-18 11:55:25 +0530

    Update Navbar.jsx

--- portfo/src/components/Navbar.jsx
+++ portfo/src/components/Navbar.jsx
@@ -1,29 +1,60 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <>
       <div className="w-screen bg-black">
-      <div className="w-full h-[6rem] flex flex-row bg-gray-600 font-bold border-b-2 border-black">
+        <div className="w-full h-24 flex flex-row bg-gray-600 font-bold border-b-2 border-black">
 
+          {/* Branding Section */}
           <div className="w-[50rem] h-full flex justify-center items-center bg-[#b4dae9] text-black px-4">
-            <div>AKASH</div>
+            <div className="text-3xl">AKASH</div>
           </div>
+
+          {/* Navigation Links */}
           <div className="flex-1 flex justify-around items-center bg-[#b4dae9] text-black">
-            <div className="w-[13rem] text-center py-2 cursor-pointer">ABOUT</div>
-            <Link to={'/'}><div className="w-[13rem] text-center py-2 cursor-pointer">PROFILE</div></Link>
-            <Link to={'/project'}><div className="w-[13rem] text-center py-2 cursor-pointer">PROJECT</div></Link>
-            <Link to={'/contact'}><div className="w-[5rem] text-center py-2 cursor-pointer">CONTACT</div></Link>
+            
+            {/* About Section */}
+            <div className="w-[13rem] text-center py-2 cursor-pointer">
+              ABOUT
+            </div>
+
+            {/* NavLink to Profile */}
+            <NavLink
+              to="/"
+              className={({ isActive }) => 
+                `w-[13rem] text-center py-2 cursor-pointer transition-colors duration-300 ${isActive ? 'font-bold text-white' : 'text-black'}`
+              }
+            >
+              PROFILE
+            </NavLink>
+
+            {/* NavLink to Project */}
+            <NavLink
+              to="/project"
+              className={({ isActive }) => 
+                `w-[13rem] text-center py-2 cursor-pointer transition-colors duration-300 ${isActive ? 'font-bold text-white' : 'text-black'}`
+              }
+            >
+              PROJECT
+            </NavLink>
+
+            {/* NavLink to Contact */}
+            <NavLink
+              to="/contact"
+              className={({ isActive }) => 
+                `w-[5rem] text-center py-2 cursor-pointer transition-colors duration-300 ${isActive ? 'font-bold text-white' : 'text-black'}`
+              }
+            >
+              CONTACT
+            </NavLink>
+
           </div>
         </div>
       </div>
-      {/* <div className="w-3/5 h-80 bg-slate-500 flex justify-center items-center text-white">
-        <div>Some content here</div>
-      </div> */}
-      
     </>
   );
-}
+};
 
 export default Navbar;
