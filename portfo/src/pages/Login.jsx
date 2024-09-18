236c3a9317a6432535aadeedbb5c7bb6194872f2
Akash-4507 <akashanand9790@gmail.com>
2024-09-18 18:58:58 +0530

    Update Login.jsx

--- portfo/src/pages/Login.jsx
+++ portfo/src/pages/Login.jsx
@@ -1,5 +1,4 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react';
 
 const Login = () => {
     const [userdata,setUserdata] = useState(
@@ -12,13 +11,18 @@ const Login = () => {
     {
         setUserdata({...userdata,[e.target.id]: e.target.value})
     }
-    console.log(userdata)
+    const handleSubmit=(e)=>
+    {
+        e.preventDefault();
+    console.log(userdata);
+
+    }
 
   return (
     <>
     <div className="h-screen w-screen bg-white flex flex-col justify-center items-center">
                  <div className="w-[40%] h-[50%] flex flex-row justify-center items-center">
-                                    <form className="w-[80%] h-[80%] flex flex-col justify-center items-center gap-4" >
+                                    <form className=" onSubmit={handleSubmit} on w-[80%] h-[80%] flex flex-col justify-center items-center gap-4" >
                                         <input type="email" id="email" placeholder="email" className="p-3 bg-[#f8f8f8] w-full font-bold outline-none active:outline-none focus:border-b-2 hover:border-purple-500" required onChange={handleChange}/>
                                         <input type="password" name="" id="password" placeholder="password" className="p-3 bg-[#f8f8f8] w-full font-bold outline-none active:outline-none focus:border-b-2 hover:border-purple-500" required onChange={handleChange}/>
                                         {/* <input type="email" name="" id="email" placeholder="email" className="p-3 bg-[#f8f8f8] w-full font-bold outline-none active:outline-none focus:border-b-2 hover:border-purple-500" required onChange={handleChange} />
@@ -31,5 +35,4 @@ const Login = () => {
  
   )
 }
-
-export default Login
+export default Login;
