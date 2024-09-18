93b3fb15e98d9515633d239fe47ca77524acb642
Akash-4507 <akashanand9790@gmail.com>
2024-09-18 19:12:29 +0530

    Update Login.jsx

--- portfo/src/pages/Login.jsx
+++ portfo/src/pages/Login.jsx
@@ -1,28 +1,35 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 
 const Login = () => {
-    const [userdata,setUserdata] = useState(
-    {
-        email:"",
-        password:""
-    }
-    )
-    const handleChange=(e)=>
-    {
-        setUserdata({...userdata,[e.target.id]: e.target.value})
-    }
-    const handleSubmit=(e)=>
-    {
-        e.preventDefault();
-    console.log(userdata);
+    // const [userdata,setUserdata] = useState(
+    // {
+    //     email:"",
+    //     password:""
+    // }
+    // )
+    // const handleChange=(e)=>
+    // {
+    //     setUserdata({...userdata,[e.target.id]: e.target.value})
+    // }
 
+    const emailref=useRef(null);
+    const passwordref=useRef(null);
+    
+    const handleLogin = (e) => {
+        e.preventDefault()
+        // console.log(userdata)
+        // console.log("hello")
+        console.log(emailref.current.value , passwordref.current.value)
     }
 
   return (
     <>
     <div className="h-screen w-screen bg-white flex flex-col justify-center items-center">
                  <div className="w-[40%] h-[50%] flex flex-row justify-center items-center">
-                                    <form className=" onSubmit={handleSubmit} on w-[80%] h-[80%] flex flex-col justify-center items-center gap-4" >
+                                    <form  onSubmit={handleLogin} className="  w-[80%] h-[80%] flex flex-col justify-center items-center gap-4" >
+                                        <input type="email" ref={emailref} id="email" placeholder="email" className="p-3 bg-[#f8f8f8] w-full font-bold outline-none active:outline-none focus:border-b-2 hover:border-purple-500" required />
+                                        <input type="password" ref={passwordref} name="" id="password" placeholder="password" className="p-3 bg-[#f8f8f8] w-full font-bold outline-none active:outline-none focus:border-b-2 hover:border-purple-500" required />
+                                        {/* <input type="email" name="" id="email" placeholder="email" className="p-3 bg-[#f8f8f8] w-full font-bold outline-none active:outline-none focus:border-b-2 hover:border-purple-500" required onChange={handleChange} />
                                         <input type="email" id="email" placeholder="email" className="p-3 bg-[#f8f8f8] w-full font-bold outline-none active:outline-none focus:border-b-2 hover:border-purple-500" required onChange={handleChange}/>
                                         <input type="password" name="" id="password" placeholder="password" className="p-3 bg-[#f8f8f8] w-full font-bold outline-none active:outline-none focus:border-b-2 hover:border-purple-500" required onChange={handleChange}/>
                                         {/* <input type="email" name="" id="email" placeholder="email" className="p-3 bg-[#f8f8f8] w-full font-bold outline-none active:outline-none focus:border-b-2 hover:border-purple-500" required onChange={handleChange} />
