25718a29ea09ef3bc3d8b00fe22a265b4502b66d
Akash-4507 <akashanand9790@gmail.com>
2024-09-20 11:13:20 +0530

    Update Profile.jsx

--- portfo/src/pages/Profile.jsx
+++ portfo/src/pages/Profile.jsx
@@ -6,9 +6,10 @@ const Profile=()=>{
      <>
      <Navbar />
      <div className="w-full h-[35rem] flex flex-row justify-center items-center">
-      <div className="w-5/5 h-[35rem] bg-[rgb(199,201,202)] flex flex-col justify-center items-center border-r-2 border-black gap-3">
-       <div className="w-[55rem] h-20 bg-[rgb(199,201,202)] text-black flex flex-col justify-center items-center font-bold rounded-md ">HI HELLO THIS IS AKASH A</div>
-      <div className="w-[55rem] h-40 bg-[rgb(199,201,202)] text-black font-bold flex flex-col justify-center items-center rounded-md">
+      <div className="w-5/5 h-[35rem] bg-[#f8f8f8] flex flex-col justify-center items-center border-b-2 border-black gap-3">
+       <div className="w-[100rem] h-[25rem] bg-[#f8f8f8] text-black flex flex-col justify-center items-center font-bold rounded-md gap-3">HI HELLO THIS IS AKASH A</div>
+       <div className="w-[70rem] h-[10rem] bg-[#f8f8f8] text-black flex flex-col justify-center items-center font-bold rounded-md gap-3">HI HELLO THIS IS AKASH A</div>
+      <div className="w-[80rem] h-[40rem] bg-[#f8f8f8] text-black font-bold flex flex-col justify-center items-center rounded-md gap-3">
         hellos
       </div>
       </div>
