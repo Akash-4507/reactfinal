492102923a6a18c28dd6671a6cd7730c4e11074a
Akash-4507 <akashanand9790@gmail.com>
2024-09-20 11:15:33 +0530

    Update Profile.jsx

--- portfo/src/pages/Profile.jsx
+++ portfo/src/pages/Profile.jsx
@@ -7,7 +7,9 @@ const Profile=()=>{
      <Navbar />
      <div className="w-full h-[35rem] flex flex-row justify-center items-center">
       <div className="w-5/5 h-[35rem] bg-[#f8f8f8] flex flex-col justify-center items-center border-b-2 border-black gap-3">
-       <div className="w-[100rem] h-[25rem] bg-[#f8f8f8] text-black flex flex-col justify-center items-center font-bold rounded-md gap-3">HI HELLO THIS IS AKASH A</div>
+       <div className="w-[100rem] h-[25rem] bg-[#f8f8f8] text-black flex flex-col justify-center items-center font-bold rounded-md gap-3">
+        <img src="" alt="akash"/>
+       </div>
        <div className="w-[70rem] h-[10rem] bg-[#f8f8f8] text-black flex flex-col justify-center items-center font-bold rounded-md gap-3">HI HELLO THIS IS AKASH A</div>
       <div className="w-[80rem] h-[40rem] bg-[#f8f8f8] text-black font-bold flex flex-col justify-center items-center rounded-md gap-3">
         hellos
